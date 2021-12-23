e28ccc4596d4dacfb1ecc316e1c1b43ef2f677f2
Vladimir <agronom.kh.ua@gmail.com>
2021-12-23 15:58:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+let incr = 10,
+	decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(++incr); префиксный инкр
+//console.log(--decr); префиксный декр
+
+//console.log(incr++); постфиксный
+//console.log(decr--); постфиксный
+
+console.log(5 % 2);
+
+console.log(2 * 4 == 8);//true
+console.log(2 * 4 == "8");//true
+console.log(2 * 4 === "8");//false так как строгое сравнение ===
+
+//----&&  это оператор "и" возвращает true если оба параметра будут true
+
+//const isChecked = true,
+//	isClose = true;
+
+//console.log(isChecked && isClose);
+
+//---- || это оператор "или" возращает true если один из параметров будут true
+
+//const isChecked = true,
+//isClose = false;
+
+//console.log(isChecked || isClose);
+
+//-----!  это оператор отрицания, делает значение обратным
+
+//const isChecked = true,
+//isClose = false;
+
+//console.log(!isChecked || isClose);
+
+
+//console.log(2 + 2 * 2 !== 8);
